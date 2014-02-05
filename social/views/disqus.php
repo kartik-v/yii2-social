2b8de4059058db9455c81d2875bfe48e6494cd88
Kartik Visweswaran <kartikv2@gmail.com>
2014-02-05 22:28:00 +0000

    Updated Disqus widget

--- social/views/disqus.php
+++ social/views/disqus.php
@@ -20,12 +20,12 @@ use yii\helpers\Html;
     <?= $variables ?>
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function() {
-        var s = document.createElement('script');
-        s.async = true;
-        s.type = 'text/javascript';
-        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-    }());
+        var dsq = document.createElement('script');
+        dsq.type = 'text/javascript';
+        dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
 </script>
 <noscript><?= $noscript ?></noscript>
 <?= $credits ?>
